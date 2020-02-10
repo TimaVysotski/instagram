2898c697639610accb549c1ee778faf368d06c5b
Timan <vysotski.tima@gmail.com>
2020-02-10 18:47:20 +0300

    DEVELOP Add TypeScript and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  extends: ['airbnb', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'max-len': 0,
+    'no-extra-semi': 0,
+    'no-confusing-arrow': 0,
+    'object-curly-newline': 0,
+    'import/extensions': 0,
+    'no-nested-ternary': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.tsx', '**/test.ts'] }],
+    '@typescript-eslint/indent': [2, 2],
+  },
 };
