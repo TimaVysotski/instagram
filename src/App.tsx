85baf96b81bf68239a673deb1304fcc3b59bf5aa
Timan <vysotski.tima@gmail.com>
2020-02-10 19:36:33 +0300

    DEVELOP Import ReactElement

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { View } from 'react-native';
 
-export default (): React.ReactElement => {
+export default (): ReactElement => {
   return (
     <View />
   );
